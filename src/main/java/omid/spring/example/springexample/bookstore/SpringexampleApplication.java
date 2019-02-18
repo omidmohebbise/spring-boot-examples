4e7b296545fdf0e1f01715f518f2744a1d8ca546
omidmohebbi <omidmohebbise@gmail.com>
2019-02-18 15:53:26 +0330

    test again book store.

--- src/main/java/omid/spring/example/springexample/bookstore/SpringexampleApplication.java
+++ src/main/java/omid/spring/example/springexample/bookstore/SpringexampleApplication.java
@@ -1,16 +1,14 @@
-package omid.spring.example.springexample;
+package omid.spring.example.springexample.bookstore;
 
-import omid.spring.example.springexample.qulifire.SimpleControllerQuailifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 public class SpringexampleApplication {
 
-    public    void main(String[] args) {
+    public static   void main(String[] args) {
         ConfigurableApplicationContext contex =  SpringApplication.run(SpringexampleApplication.class, args);
     }
 
