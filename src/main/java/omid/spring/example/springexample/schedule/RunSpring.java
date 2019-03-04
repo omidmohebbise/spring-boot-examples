3349933fe962be2e39b1c5fc394b81ec6a62364d
omidmohebbi <omidmohebbise@gmail.com>
2019-03-04 13:52:05 +0330

    Read a proprety from application.properties in simple component

---
+++ src/main/java/omid/spring/example/springexample/schedule/RunSpring.java
@@ -0,0 +1,17 @@
+package omid.spring.example.springexample.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+//@SpringBootApplication
+//@EnableScheduling
+public class RunSpring {
+    public  static   void main(String[] args) {
+        ConfigurableApplicationContext contex =  SpringApplication.run(RunSpring.class, args);
+    }
+
+
+}
