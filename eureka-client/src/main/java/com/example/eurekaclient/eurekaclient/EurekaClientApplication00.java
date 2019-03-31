55fc29adb4cd348c07d6f28a78a1bc6187cfe156
omidmohebbi <omidmohebbise@gmail.com>
2019-03-31 13:39:41 +0430

    I worked on service registry and discovery. In this version I registered a simple controller in eureka-client project and discovered this url in eureka-client-01. It was beautiful :)

--- eureka-client/src/main/java/com/example/eurekaclient/eurekaclient/EurekaClientApplication00.java
+++ eureka-client/src/main/java/com/example/eurekaclient/eurekaclient/EurekaClientApplication00.java
@@ -1,12 +1,19 @@
 package com.example.eurekaclient.eurekaclient;
 
+import com.example.eurekaclient.eurekaclient.model.Person;
+import javafx.scene.PerspectiveCamera;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientApplication00 {
 
@@ -14,12 +21,27 @@ public class EurekaClientApplication00 {
         SpringApplication.run(EurekaClientApplication00.class, args);
     }
 
-    @EnableEurekaClient
+
     @RestController
     public class SimpleController {
         @RequestMapping("/helloworld")
         public String getHelloWorld() {
-            return "Hello World";
+            return "Hello Worldss";
+        }
+
+
+        @RequestMapping("/persons")
+        public List<Person> getPersons() {
+
+            List<Person> persons = new ArrayList<Person>();
+            for (int i = 0; i < 20; i++) {
+                Person person = new Person();
+                person.setName("omid" + " " + i);
+                person.setFamily("mohebbi" + " " + i);
+                persons.add(person);
+            }
+            return persons;
+
         }
     }
 
