282d607d86c4d2fe2c943dcc1b0a475651ef013f
omidmohebbi <omidmohebbise@gmail.com>
2019-04-10 13:31:43 +0430

    Circuit Breaker is ok in this version.

---
+++ hystrix-server/src/main/java/com/example/hystrix/hystrixserver/HystrixServletDefinitions.java
@@ -0,0 +1,18 @@
+package com.example.hystrix.hystrixserver;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixServletDefinitions {
+    @Bean(name = "hystrixRegistrationBean")
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                new HystrixMetricsStreamServlet(), "/hystrix.stream");
+        registration.setName("hystrixServlet");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+}
