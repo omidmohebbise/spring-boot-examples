4f3f511e2973df076e21f57874d63835b0305a51
omidmohebbi <omidmohebbise@gmail.com>
2019-06-01 11:52:27 +0430

    Circuit Breaker is ok in this version.

---
+++ temp/src/main/java/com/example/temp/temp/Test.java
@@ -0,0 +1,42 @@
+package com.example.temp.temp;
+
+import java.sql.Blob;
+
+public class Test {
+    private Long deploymentPlanId;
+    private java.sql.Date datDeploymentDeppl;
+    private String desUserDeploymentDeppl;
+    private String numVersionDeppl;
+    private Blob filEarDeppl;
+    private String lkpStaDeploymentDeppl;
+    private String namEnvDestinationDeppl;
+    private Long applsApplicationSystemId;
+    private String groupIdeGrpGroup;
+    private String usrsIdeUserUsrs;
+    private String desDeploymentDeppl;
+    private Long flgIgnoreLimitationDeppl;
+    private Long deplmDeploymentLimitationId;
+    private Long flgIsLibraryDeppl;
+    private String namEarDeppl;
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "deploymentPlanId=" + deploymentPlanId +
+                ", datDeploymentDeppl=" + datDeploymentDeppl +
+                ", desUserDeploymentDeppl='" + desUserDeploymentDeppl + '\'' +
+                ", numVersionDeppl='" + numVersionDeppl + '\'' +
+                ", filEarDeppl=" + filEarDeppl +
+                ", lkpStaDeploymentDeppl='" + lkpStaDeploymentDeppl + '\'' +
+                ", namEnvDestinationDeppl='" + namEnvDestinationDeppl + '\'' +
+                ", applsApplicationSystemId=" + applsApplicationSystemId +
+                ", groupIdeGrpGroup='" + groupIdeGrpGroup + '\'' +
+                ", usrsIdeUserUsrs='" + usrsIdeUserUsrs + '\'' +
+                ", desDeploymentDeppl='" + desDeploymentDeppl + '\'' +
+                ", flgIgnoreLimitationDeppl=" + flgIgnoreLimitationDeppl +
+                ", deplmDeploymentLimitationId=" + deplmDeploymentLimitationId +
+                ", flgIsLibraryDeppl=" + flgIsLibraryDeppl +
+                ", namEarDeppl='" + namEarDeppl + '\'' +
+                '}';
+    }
+}
