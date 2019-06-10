ebf5ac2bb2b63a750305e2ee4e184629cf362f7b
omidmohebbi <omidmohebbise@gmail.com>
2019-06-10 14:56:54 +0430

    Circuit Breaker is ok in this version.

--- zuul-server/src/main/java/com/example/zullserver/FirstFilter.java
+++ zuul-server/src/main/java/com/example/zullserver/FirstFilter.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class FirstFilter extends ZuulFilter {
     private static Logger log = LoggerFactory.getLogger(FirstFilter.class);
 
