b51ca3db9565db2e3c82f02dbf9b95519467e45c
omidmohebbise@gmail.com <Omid@1234>
2019-07-29 16:59:52 +0430

    H2 db tested in this commit

---
+++ dev-tools/src/main/java/com/omid/dev/tools/devtools/DevToolsApplication.java
@@ -0,0 +1,15 @@
+package com.omid.dev.tools.devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevToolsApplication.class, args);
+    }
+
+
+
+}
