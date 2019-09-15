cfabe01d4cfac0db4d6e156ed60411d0ea618247
Omid Mohebbi <omid.mohebbi@rdsysco.com>
2019-09-15 17:01:31 +0430

    simple-read-data-from-db updated: - packages updated - repositories updated.

--- simple-read-data-from-db/src/main/java/com/example/bookstore/repositories/AuthorsRepository.java
+++ simple-read-data-from-db/src/main/java/com/example/bookstore/repositories/AuthorsRepository.java
@@ -2,12 +2,13 @@ package com.example.bookstore.repositories;
 
 
 import com.example.bookstore.model.Author;
+import org.springframework.data.repository.CrudRepository;
 
 interface MyInterface {
      Iterable<Author> findByName(String omid);
 }
 
-public interface AuthorsRepository extends MyInterface{//,CrudRepository<Author,Long> {
+public interface AuthorsRepository extends MyInterface, CrudRepository<Author,Long> {
 
 
 
