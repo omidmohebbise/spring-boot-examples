4553e199fbebc066d107eb56d41c8c67fb013cee
Omid Mohebbi <omid.mohebbi@rdsysco.com>
2019-11-03 09:55:31 +0330

    Delete test-api module

--- eureka-server/src/main/java/com/example/eureka/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eureka/eureka/EurekaServerApplication.java
@@ -2,7 +2,6 @@ package com.example.eureka.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,7 @@ public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
-    @EnableEurekaClient
+    //@EnableEurekaClient
     @RestController
     public class SimpleController {
         @RequestMapping("/helloworld")
