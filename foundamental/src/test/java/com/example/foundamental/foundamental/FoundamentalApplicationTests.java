a8f94dc8bd5448012c795be9601c4d2a90e26bd6
Omid Mohebbi <omid.mohebbi@rdsysco.com>
2019-11-12 15:22:52 +0330

    modify POMs

---
+++ foundamental/src/test/java/com/example/foundamental/foundamental/FoundamentalApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.foundamental.foundamental;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundamentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+}
