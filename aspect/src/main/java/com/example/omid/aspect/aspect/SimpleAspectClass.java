fda8d2527f06c7d597cfa2565a60e26793458cb5
Omid Mohebbi <omid.mohebbi@rdsysco.com>
2019-12-24 08:41:33 +0330

    Add new aspect with other pattern.

--- aspect/src/main/java/com/example/omid/aspect/aspect/SimpleAspectClass.java
+++ aspect/src/main/java/com/example/omid/aspect/aspect/SimpleAspectClass.java
@@ -15,12 +15,11 @@ public class SimpleAspectClass {
     public void logBeforCallRest(JoinPoint joinPoint){
         System.out.println("Hellooo");
     }
-/*
-    @AfterReturning(
-            pointcut="com.example.omid.aspect.aspect.SimpleRestController.printHelloWorld()",
-            returning="retVal")
-    public void doAccessCheck(Object retVal) {
-        // ...
+
+    @Before("execution(* com.example.omid.aspect.aspect.SimpleRestController.*()))")
+    public void logBeforCallRest1(JoinPoint joinPoint){
+        System.out.println("Hellooo1");
     }
-*/
+
+
 }
