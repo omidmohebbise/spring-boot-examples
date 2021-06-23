5434606c6a2c3b0b3fa17d6349ff9c1cce9fddbf
Omid Mohebbi <omid.mohebbi@rdsysco.com>
2021-06-23 17:18:02 +0430

    Implement tests powering by mockito.

---
+++ test-practicing/mockito-starter/src/main/java/com/spdeveloper/mockitostarter/MockitoStarterApplication.java
@@ -0,0 +1,13 @@
+package com.spdeveloper.mockitostarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoStarterApplication.class, args);
+    }
+
+}
